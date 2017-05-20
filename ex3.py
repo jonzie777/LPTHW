e05f2f16cb57c0e5415a3d06673f89cb24578b92
jonzie777 <mathewallenjones@gmail.com>
2017-05-20 16:12:51 -0700

    LPTHW

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# First statement
+print "I will not count my chickens:"
+# Counting the Hens
+print "Hens", 25 + 30/6
+# Counting the Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print statement
+print "Now i will count the eggs:"
+# Math to count the eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Print statement
+print "Is it true that 3 + 2 < 5 - 7?"
+# Math for true or false
+print 3 + 2 < 5 - 7
+# Printing a question then doing math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+# Printing a statement
+print "Oh, that's why it's False."
+# Printing a statement
+print "How about some more."
+# Printing a statement then doing math
+print "Is it greater?", 5 > -2
+# Printing a statement then doing math
+print "Is it greater or equal?", 5 >= -2
+# Printing a statement then doing math
+print "Is it less or equal?", 5 <= -2
