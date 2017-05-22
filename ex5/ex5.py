3cd3e3fa7592dfb1aed01a4bf1a19c42167ad8ff
jonzie777 <mathewallenjones@gmail.com>
2017-05-21 21:29:12 -0700

    ex5

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -1,17 +1,22 @@
-my_name = 'Mathew A. Jones'
-my_age = 31 # this is my actual age
-my_height = 6.0 # inches
-my_weight = 285 # lbs
-my_eyes = 'Hazel'
-my_teeth = 'White..ish'
-my_hair = 'Brown'
+name = 'Mathew A. Jones'
+age = 31 # this is my actual age
+height = 6.0 # inches
+weight = 285 # lbs
+eyes = 'Hazel'
+teeth = 'White..ish'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually thats pretty heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d. %d. and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d. %d. and %d I get %d." % (age, height, weight, age + height + weight)
+
+# Convert pounds to kilo's
+pounds = 280
+kilos = pounds * 0.45359237
+print "%r pounds equals %r kilos." % (pounds, kilos)
